016e695eccb1da7a5a50f70003ef8ad4cf92ef68
simonecampilli <74252963+simonecampilli@users.noreply.github.com>
2022-11-09 20:45:12 +0100

    Add files via upload

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,72 @@
+from snippets.models import Snippet,Prova
+from snippets.serializers import SnippetSerializer, ProvaSerializer
+from rest_framework import generics
+
+from rest_framework import permissions
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+        'prova': reverse('prova-list', request=request, format=format)
+    })
+
+from rest_framework import renderers
+from snippets.permissions import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+
+#unica view per userlist e userdetail
+from rest_framework import viewsets
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+#sostituzione snippet list, details e highlights
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import permissions
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticated,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ProvaViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Prova.objects.all()
+    serializer_class = ProvaSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
